0cb35d4580e3640b84f9865bcd8f7750247c16cb
HannahS62 <hannah.smith6@msn.com>
2024-08-05 16:27:21 +0100

    4 tests passing

--- index.test.js
+++ index.test.js
@@ -1,22 +1,42 @@
 // install dependencies
-const { execSync } = require('child_process');
-execSync('npm install');
-execSync('npm run seed');
+const { execSync } = require("child_process");
+execSync("npm install");
+execSync("npm run seed");
 
-const request = require("supertest")
-const { db } = require('./db/connection');
-const { Musician } = require('./models/index')
-const app = require('./src/app');
+const request = require("supertest");
+const { db } = require("./db/connection");
+const { Musician, Band } = require("./models/index");
+const app = require("./src/app");
 const seedMusician = require("./seedData");
 
+beforeAll(async () => {
+  await db.sync({ force: true });
+});
 
-describe('./musicians endpoint', () => {
-    // Write your tests here
-    
-    
+afterEach(async () => {
+  await db.truncate({ cascade: true }); // cascade will remove assosiated data
+});
 
+describe("./musicians endpoint", () => {
+  it("returns 200 status code", async () => {
+    const response = await request(app).get("/musicians");
+    expect(response.statusCode).toBe(200);
+  });
+  it("returns an array of Musician", async () => {
+    const response = await request(app).get("/musicians");
+    const responseData = JSON.parse(response.text);
+    expect(Array.isArray(responseData)).toBe(true);
+  });
+});
 
-
-
-    
-})
+describe("./bands endpoint", () => {
+  it("returns 200 status code", async () => {
+    const response = await request(app).get("/bands");
+    expect(response.statusCode).toBe(200);
+  });
+  it("returns an array of Band", async () => {
+    const response = await request(app).get("/bands");
+    const responseData = JSON.parse(response.text);
+    expect(Array.isArray(responseData)).toBe(true);
+  });
+});
